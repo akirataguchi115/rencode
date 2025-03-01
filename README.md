7bab83a723a4332acdd1664d9e3c8638cd7acdb4
Akira Taguchi <Akira Taguchi>
2025-03-01 20:03:08 +0200

    quick save

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ooh maybe migrate to typescript
+
+this turns out to be pretty manual labor since wasm stuff can be very hard to get working and chadgpt has no idea / history on people getting it to work
+
+https://i.ylilauta.org/15/0e/150e2dcac76c50d1.mp4
+
+gifsane is a bad example cuz its a webapp rather than a browser extension
+
+ok i have to use content script for ffmpeg since background script cannot do any heavy lifting
+
+however i need background script for registering the contextmenu
+
+immaculate i have almost managed to send a message from bg script to defined content script quick saving here
--- background-script.js
+++ background-script.js
@@ -1,13 +1,30 @@
-browser.contextMenus.create(
-  {
-    id: "log-selection",
-    title: "Encode to H.264 and save",
-    contexts: ["video"],
-  });
+"use strict";
 
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
 
-  browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "log-selection") {
-      console.log('Video URL: ', info.srcUrl)
-    }
+browser.contextMenus.create({
+  id: "encode-video",
+  title: "Encode to H.264 and save",
+  contexts: ["video"],
+});
+
+function sendMessageToTabs(tabs) {
+  for (const tab of tabs) {
+    browser.tabs
+      .sendMessage(tab.id, { greeting: "Hi from background script" })
+      .then((response) => {
+        console.log("Message from the content script:");
+        console.log(response.response);
+      })
+      .catch(onError);
+  }
+}
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === "encode-video") {
+    console.log("Sending encode request:", info.srcUrl);
+    sendMessageToTabs()
+  }
 });
